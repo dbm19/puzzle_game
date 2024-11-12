924d9fc6a1964eb61381f51f213bfbda5ad0fabd
Daniel Mau <daniel.b.mau@gmail.com>
2024-11-12 15:14:23 -0500

    Added diagonal piece

--- tile_map.gd
+++ tile_map.gd
@@ -2,18 +2,26 @@ extends TileMapLayer
 
 var ortho_piece_scene
 var ortho_piece_instance
+var diagon_piece_scene
+var diagon_piece_instance
 var dot_scene
 var dot_instance
-var mouse_coordinates
-var ortho_tiles
+var m_c #mouse_coordinates
+var current_piece
+var modified_tiles
 var all_tiles_coordinates
 var all_tiles = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ortho_piece_scene = preload("res://ortho_piece.tscn")
+	ortho_piece_instance = ortho_piece_scene.instantiate()
+	diagon_piece_scene = preload("res://diagon_piece.tscn")
+	diagon_piece_instance = diagon_piece_scene.instantiate()
 	dot_scene = preload("res://dot.tscn")
 
+	current_piece = "ortho_piece"
+	
 	var dictionary_index = 0
 	all_tiles_coordinates = get_used_cells()
 	for n in all_tiles_coordinates:
@@ -25,12 +33,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#Global.tile_center.add_child(ortho_piece_instance)
+	if Input.is_action_just_pressed("switch_piece"):
+		if current_piece == "ortho_piece":
+			current_piece = "diagon_piece"
+		else:
+			current_piece = "ortho_piece"
+
 	if Input.is_action_just_pressed("add_piece") && Global.mouse_is_over:
-		ortho_piece_instance = ortho_piece_scene.instantiate()
-		Global.tile_center.add_child(ortho_piece_instance)
-		ortho_tiles = get_surrounding_cells(mouse_coordinates)
-		
-		for n in ortho_tiles:
+		if current_piece == "ortho_piece":
+			modified_tiles = get_surrounding_cells(m_c)
+		else:
+			modified_tiles = [Vector2i(m_c.x - 1, m_c.y - 1), Vector2i(m_c.x + 1, m_c.y - 1), Vector2i(m_c.x - 1, m_c.y + 1), Vector2i(m_c.x + 1, m_c.y + 1)]
+			
+		for n in modified_tiles:
 			if all_tiles.has(n):
 				#set_cell(n, 0, Vector2i(0, 0), 2) #0 is the TileSet id, 2 is the Tile id
 				#print(local_to_map(Global.tiles_dictionary[n].get_node("TileCenter").global_position))
@@ -43,7 +59,7 @@ func _process(delta: float) -> void:
 				pass
 
 func get_cell_coordinates():
-	mouse_coordinates = local_to_map(get_viewport().get_mouse_position())
+	m_c = local_to_map(get_viewport().get_mouse_position())
 
 func _on_tile_map_area_mouse_exited() -> void:
 	Global.mouse_is_over = false
